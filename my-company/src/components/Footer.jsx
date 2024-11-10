37c11c59be6019f72a90c45f10d29bc9d152d6d5
Qhojoblinks-7 <immanueleshun9@gmail.com>
2024-11-10 23:58:59 +0000

    i added justifyContent: 'center'

--- my-company/src/components/Footer.jsx
+++ my-company/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 // src/Footer.jsx
 function Footer() {
     return (
-      <footer style={{ padding: '10px', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
+      <footer style={{ padding: '10px', backgroundColor: '#333', color: '#fff', textAlign: 'center', justifyContent: 'center' }}>
         <p>&copy; {new Date().getFullYear()} My Company</p>
       </footer>
     );
