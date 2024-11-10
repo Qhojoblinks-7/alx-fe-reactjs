f4e1a5f6954ce38f4d7ad58a037abc7c09422d33
Qhojoblinks-7 <immanueleshun9@gmail.com>
2024-11-10 23:53:46 +0000

    done

--- my-company/src/components/About.jsx
+++ my-company/src/components/About.jsx
@@ -4,6 +4,7 @@ function About() {
         <h1>About Us</h1>
         <p>Our company has been providing top-notch services since 1990. We specialize in various fields including technology, marketing, and consultancy.</p>
       </div>
+      
     );
   }
 
