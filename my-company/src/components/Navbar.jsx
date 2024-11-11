af7deddb06080d19fd7e1d07c551102427f57ca7
Qhojoblinks-7 <immanueleshun9@gmail.com>
2024-11-11 00:00:34 +0000

    i add  justifyContent: 'center'

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function Navbar(){
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff', justifyContent: 'center' }}>
     <ul style={{ listStyle: 'none', display: 'flex', gap: '10px' }}>
       <li><Link to="/" style={{ color: '#ffffff', textDecoration: 'none' }}>Home</Link></li>
       <li><Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link></li>
