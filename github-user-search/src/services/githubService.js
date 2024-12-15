ec2116acc7288b95806cd9d0702120e3f5ec0a74
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2024-12-15 22:32:08 +0000

    updated

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,37 +1,29 @@
 import axios from 'axios';
 
-
 // Get the token from environment variables
-const GITHUB_TOKEN = process.env.VITE_GITHUB_API_KEY;
-
-// if (typeof process !== 'undefined') {
-//     console.log(process.env.NODE_ENV);
-//   } else {
-//     console.log("Environment variable not available.");
-//   }
-  
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_API_KEY;
 
 const githubService = async (searchTerm, location = '', minRepos = '', page = 1) => {
-    try {
-      const query = [
-        searchTerm ? `${searchTerm} in:login` : '',
-        location ? `location:${location}` : '',
-        minRepos ? `repos:>${minRepos}` : '',
-      ]
-        .filter(Boolean)
-        .join('+');
-  
-      const response = await axios.get('https://api.github.com/search/users', {
-        params: { q: query, page },
-        headers: {
-          Authorization: `Bearer ${GITHUB_TOKEN}`,
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching data from GitHub API:', error);
-      throw error;
-    }
-  };
-  
-  export default githubService;
+  try {
+    const query = [
+      searchTerm ? `${searchTerm} in:login` : '',
+      location ? `location:${location}` : '',
+      minRepos ? `repos:>${minRepos}` : '',
+    ]
+      .filter(Boolean)
+      .join('+');
+
+    const response = await axios.get('https://api.github.com/search/users', {
+      params: { q: query, page },
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data from GitHub API:', error);
+    throw error;
+  }
+};
+
+export default githubService;
